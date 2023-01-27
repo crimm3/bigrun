3b3da21681a63c135740d104d2b452379f0b685f
crimm3 <33089256+crimm3@users.noreply.github.com>
2023-01-27 22:08:56 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,6 @@ from scipy.stats import norm
 
 st.title('ビッグラン上位パーセント')
 kinikura = st.text_input('金イクラの数')
-st.write('あなたは上位:{}'.format(norm.cdf(int(kinikura), loc='88', scale='29.75')))
-st.write('偏差値:{}'.format((int(kinikura) - 88)/29.75*10)+50)
+if kinikura is not None:
+  st.write('あなたは上位:{}'.format(norm.cdf(int(kinikura), loc='88', scale='29.75')))
+  st.write('偏差値:{}'.format((int(kinikura) - 88)/29.75*10)+50)

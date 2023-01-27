65e05fae74d30bcae420a56d3ed899e5cf7de78e
crimm3 <33089256+crimm3@users.noreply.github.com>
2023-01-27 21:58:41 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,9 @@
-x = 10
-'x: ', x
+import streamlit as st
+import plotly.express as px
+import numpy as np
+from scipy.stats import norm
+
+st.title('ビッグラン上位パーセント')
+kinikura = st.text_input('金イクラの数')
+st.write('あなたは上位:{}'.format(norm.cdf(int(kinikura), loc='88', scale='29.75')))
+st.write('偏差値:{}'.format((int(kinikura) - 88)/29.75*10)+50)

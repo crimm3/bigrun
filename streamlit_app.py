d2c1703c5413664005c7c1b8a9f6e1b7a2a1d503
crimm3 <33089256+crimm3@users.noreply.github.com>
2023-01-27 22:22:26 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,6 @@ kinikura = st.text_input('金イクラの数')
 
 if st.button('計算'):
   your_percent = norm.cdf(int(kinikura), loc=88, scale=29.75)
-  st.write('あなたは上位:{}'.format(round((1 - your_percent) * 100, 2)))
+  st.write('あなたは上位:{}'.format(round((1 - your_percent) * 100, 2)) + ' %')
   deviation_value = ((int(kinikura) - 88) / 29.75 * 10) + 50
   st.write('偏差値:{}'.format(round(deviation_value), 2))

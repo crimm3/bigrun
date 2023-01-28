3b27ac8bca4d1550aba243956d4ccccdfb81ab96
crimm3 <33089256+crimm3@users.noreply.github.com>
2023-01-28 22:17:46 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,13 +9,12 @@ stdev = 29.75
 if st.button('計算'):
   your_percent = norm.cdf(int(kinikura), loc=mean, scale=stdev)
   if 1 - your_percent >= 0.05:
-    
     st.write(
             '<span style="color:red;background:pink">あなたは上位:{} %</span>'.format(round((1 - your_percent) * 100, 2)),
               unsafe_allow_html=True)
     deviation_value = ((int(kinikura) - mean) / stdev * 10) + 50
     st.write('偏差値:{}'.format(round(deviation_value), 2))
-   else:
+  else:
     st.write('あなたは上位:{}'.format(round((1 - your_percent) * 100, 2)) + ' %')
     deviation_value = ((int(kinikura) - mean) / stdev * 10) + 50
     st.write('偏差値:{}'.format(round(deviation_value), 2))

ef5335f25382b73b5f807b680c5c0d5121784d8d
crimm3 <33089256+crimm3@users.noreply.github.com>
2023-02-23 00:46:01 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,8 @@ mean = 88
 stdev = 29.75
 if st.button('計算'):
   your_percent = norm.cdf(int(kinikura), loc=mean, scale=stdev)
+  with open('kinikura.csv', mode='a') as f:
+    f.write(str(kinikura) + '\n')
   if 1 - your_percent <= 0.05:
     st.write(
             '<span style="color:red;background:pink">あなたは上位:{} %</span>'.format(round((1 - your_percent) * 100, 2)),
